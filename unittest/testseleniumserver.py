449f710d5c68a9df90f2812cab38c3069ea73332
Daniel Wagner <daniel.wagner@1und1.de>
2010-03-10 17:06:54 +0100

    Unit tests for the seleniumserver module

---
+++ unittest/testseleniumserver.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+import sys, os, time
+import unittest
+
+seleniumConfig = {
+    "seleniumHost" : "http://localhost",
+    "seleniumPort" : 4444,
+    "seleniumDir" : "/home/dwagner/workspace/qooxdoo-schlund/project/testing/selenium/current",
+    "seleniumServerJar" : "selenium-server.jar"
+}
+
+sys.path.append(os.path.abspath(".."))
+import seleniumserver
+
+class TestSeleniumServer(unittest.TestCase):    
+    def setUp(self):
+        self.selServ = seleniumserver.SeleniumServer(seleniumConfig)
+    
+    def testStart(self):
+        self.assertFalse(self.selServ.isRunning())
+        self.selServ.start()
+        self.assertTrue(self.selServ.isRunning())
+    
+    def testStop(self):
+        self.selServ.start()
+        self.selServ.stop()
+        self.assertFalse(self.selServ.isRunning())
+    
+    def testKill(self):
+        self.selServ.start()
+        self.selServ.kill()
+        time.sleep(5)
+        self.assertFalse(self.selServ.isRunning())
+    
+    def tearDown(self):
+        self.selServ.stop()
+        del self.selServ
+
+if __name__ == '__main__':
+    unittest.main()
