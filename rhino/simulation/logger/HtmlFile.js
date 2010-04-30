274b01ffe3422439b3b07d0179227ffe1c39e097
Daniel Wagner <daniel.wagner@1und1.de>
2010-04-30 14:20:37 +0200

    Logger using custom format readable by SimulationLogParser.py

---
+++ rhino/simulation/logger/HtmlFile.js
@@ -0,0 +1,84 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2006-2010 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Daniel Wagner (d_wagner)
+
+************************************************************************ */
+
+if (!simulation.logger) {
+  simulation.logger = {}
+}
+
+simulation.logger.HtmlFile = function(filePath, prefix)
+{
+  if (!filePath) {
+    throw new Error("simulation.logger.File: No log file path specified!");
+  }
+  this.LOGFILEPATH = filePath || null;
+  this.PREFIX = prefix || "";
+  this.PREFIX += '<div class="qxappender"><div class="level-';
+  this.SUFFIX = '</div></div>';
+  
+  this.__write = function(msg)
+  {
+    var logFile = this.__getLogFile(this.LOGFILEPATH);
+    logFile.write(msg);
+    logFile.newLine();
+    logFile.close();
+  };
+   
+  this.log = function(logMessage, level)
+  {
+    var msg = logMessage ? logMessage : "";
+    var lvl = level ? level : "debug";
+    msg = this.PREFIX + lvl + '">' + msg + this.SUFFIX;
+    this.__write(msg);
+  };
+  
+  this.debug = function(logMessage)
+  {
+    if (logMessage) {
+      this.log(logMessage, "debug");
+    }
+  };
+  
+  this.info = function(logMessage)
+  {
+    if (logMessage) {
+      this.log(logMessage, "info");
+    }
+  };
+  
+  this.warn = function(logMessage)
+  {
+    if (logMessage) {
+      this.log(logMessage, "warn");
+    }
+  };
+  
+  this.error = function(logMessage)
+  {
+    if (logMessage) {
+      this.log(logMessage, "error");
+    }
+  };
+  
+  this.__getLogFile = function(filePath)
+  {
+    var fstream = new java.io.FileWriter(filePath, true);
+    var out = new java.io.BufferedWriter(fstream);
+    return out;
+  };
+};
